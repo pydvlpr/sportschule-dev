476bee4a238b05bc947e1c462074c93f4be6daf6
Martin Eigenbrodt <pydvlpr@gmail.com>
2018-10-19 21:49:10 +0200

    URLs für Kurs- und Trainer-Listen ergänzt

--- kursverwaltung/urls.py
+++ kursverwaltung/urls.py
@@ -2,6 +2,11 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    # index-Page
     path('',views.index, name='index'),
-    
+
+    # Kurs-Liste
+    path('kurse/',views.kurs_liste, name='kurs_liste'),
+    path('trainer/',views.trainer_liste, name='trainer_liste'),
+
 ]
