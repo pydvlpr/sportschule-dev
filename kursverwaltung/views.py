22172218b648fc52690aefbfbdd7ff6a1e6ef6ce
Martin Eigenbrodt <pydvlpr@gmail.com>
2018-10-19 21:48:55 +0200

    Exemplarische Views für Kurs- und Trainer-Listen ergänzt

--- kursverwaltung/views.py
+++ kursverwaltung/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import get_object_or_404,render, redirect
 # Index-Site
 def index(request):
     return render(request, 'kursverwaltung/index.html')
+
+def kurs_liste(request):
+    #kurs_liste = Kurse.objects.order_by('id')
+    #context = {'kurs_liste':course_list}
+    #return render(request, 'kursverwaltung/kurs-liste.html',context)
+    return render(request, 'kursverwaltung/kurs-liste.html')
+
+def trainer_liste(request):
+    #kurs_liste = Kurse.objects.order_by('id')
+    #context = {'kurs_liste':course_list}
+    #return render(request, 'kursverwaltung/kurs-liste.html',context)
+    return render(request, 'kursverwaltung/trainer-liste.html')
