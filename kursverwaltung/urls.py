cec570487e3d7fdfefdaf72611c5d7795923ea29
Martin Eigenbrodt <pydvlpr@gmail.com>
2018-10-20 23:37:50 +0200

    Zertifizierung Template und View erstellt, url angepasst

--- kursverwaltung/urls.py
+++ kursverwaltung/urls.py
@@ -7,9 +7,15 @@ urlpatterns = [
 
     # Kurs-Liste
     path('kurse/',views.kurs_liste, name='kurs_liste'),
+
+    #urls für Übersichten
     path('trainer/',views.trainer_liste, name='trainer_liste'),
     path('kunden/',views.kunden_liste, name='kunden_liste'),
     path('buchungen/',views.buchungen_liste, name='buchungen_liste'),
     path('raeume/',views.raum_liste, name='raum_liste'),
+    path('zertifizierungen/',views.zertifizierung_liste, name='zertifizierung_liste'),
+
+    # urls Kunden Formulare
+    #path('kunden/add/', KundeErstellen.as_view(), name="kunde-erstellen"),
 
 ]
--- kursverwaltung/views.py
+++ kursverwaltung/views.py
@@ -1,4 +1,36 @@
 from django.shortcuts import get_object_or_404,render, redirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.detail import DetailView
+
+from .models.kunde import Kunde
+from .models.trainer import Trainer
+from .models.kurs import Kurs
+from .models.raum import Raum
+from .models.buchung import Buchung
+from .models.zertifizierung import Zertifizierung
+
+
+class KundeErstellen(CreateView):
+    model = Kunde
+    fiels = [   'nachname', 'vorname', 'strasse',
+                'hausnummer', 'plz', 'stadt',
+                'handy','festnetz','fax', 'e_mail',
+                'ansprechpartner'
+             ]
+
+class KundeAktualisieren(UpdateView):
+     model = Kunde
+     fiels = [   'nachname', 'vorname', 'strasse',
+                 'hausnummer', 'plz', 'stadt',
+                 'handy','festnetz','fax', 'e_mail',
+                 'ansprechpartner'
+              ]
+
+
+class KundeLoeschen(DeleteView):
+    model = Kunde
+    success_url = reverse_lazy('kunden_liste')
 
 # Index-Site
 def index(request):
@@ -38,3 +70,12 @@ def raum_liste(request):
     #context = {'kurs_liste':course_list}
     #return render(request, 'kursverwaltung/kurs-liste.html',context)
     return render(request, 'kursverwaltung/raum-liste.html')
+    # Kunden Views
+
+# Zertfifizierungen views
+def zertifizierung_liste(request):
+    #kurs_liste = Kurse.objects.order_by('id')
+    #context = {'kurs_liste':course_list}
+    #return render(request, 'kursverwaltung/kurs-liste.html',context)
+    return render(request, 'kursverwaltung/zertifizierung-liste.html')
+    # Kunden Views
