15840246f0faf399908daf66c1b2e7d7ffef6e16
Martin Eigenbrodt <pydvlpr@gmail.com>
2018-10-23 19:03:58 +0200

    Starter Script für Django-Dev-Server angepasst. Änderungen der Datenmodelle werden jetzt zunächst erkannt und bei migration verarbeitet

--- run_django_server.sh
+++ run_django_server.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+#############################################################
+#                                                           #
+# Helper Script zum Starten des Django Servers              #
+# Vor dem Start werden Änderungen der Datenmodelle erkannt  #
+# und die Datenbank aktualisiert                            #
+#                                                           #
+#                                                           #
+#############################################################
 
+echo "Änderungen der Datenmodelle erkennen und für DB-Migration vorbereiten"
+python manage.py makemigrations
+
+echo "Migration von Änderungen der Datenmnodelle"
 python manage.py migrate
+
+echo "Server starten..."
 python manage.py runserver
 
